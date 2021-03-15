175c2f37e7d86c41cad6e49231eb15a23b3a4ca5
Ibrahim KIVANC <ikivanc@hotmail.com>
2021-03-15 18:21:00 +0300

    connection_string is provided for AzureExporter

--- azure_monitor/flask_sample/config.py
+++ azure_monitor/flask_sample/config.py
@@ -9,14 +9,14 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     INSTRUMENTATION_KEY = os.environ.get('APPINSIGHTS_INSTRUMENTATIONKEY') or \
-        '<your-ikey-here>'
+       '<your-ikey-here>'
     CONNECTION_STRING = 'InstrumentationKey=' + INSTRUMENTATION_KEY
     sampler = 'opencensus.trace.samplers.ProbabilitySampler(rate=1.0)'
     OPENCENSUS = {
         'TRACE': {
             'SAMPLER': sampler,
-            'EXPORTER': 'opencensus.ext.azure.trace_exporter.AzureExporter('
-            + CONNECTION_STRING + ')',
+            'EXPORTER': 'opencensus.ext.azure.trace_exporter.AzureExporter(connection_string="'
+            + CONNECTION_STRING + '")',
             'BLACKLIST_PATHS': ['blacklist'],
         }
     }
